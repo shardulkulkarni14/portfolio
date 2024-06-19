bec92f8d3444a4b797c99cb9caf4c6bbd0e2298b
shardulkulkarni14 <shardul.ask@gmail.com>
2024-06-19 22:03:49 +0200

    Reduced padding in nav bar and changed menu option from fixed to dyanmic for mobile view

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,34 @@
-function toggleMenu() {
-    const navbarLinks = document.getElementById('navbar-links');
-    if (navbarLinks.style.display === 'block') {
-        navbarLinks.style.display = 'none';
-    } else {
-        navbarLinks.style.display = 'block';
-    }
-}
-
-window.addEventListener('scroll', () => {
-    const docHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    const scrolled = (window.scrollY / docHeight) * 100;
-    document.getElementById('progress-bar').style.width = `${scrolled}%`;
-});
-
 document.addEventListener('DOMContentLoaded', function() {
-    const sections = document.querySelectorAll('.section');
+    // Toggle the mobile menu
+    function toggleMenu() {
+        const navbarLinks = document.getElementById('navbarNav');
+        if (navbarLinks.classList.contains('show')) {
+            navbarLinks.classList.remove('show');
+        } else {
+            navbarLinks.classList.add('show');
+        }
+    }
 
+    // Add event listener to each nav link to close the menu when clicked
+    const navLinks = document.querySelectorAll('.navbar-nav .nav-link');
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            const navbarCollapse = document.getElementById('navbarNav');
+            if (navbarCollapse.classList.contains('show')) {
+                navbarCollapse.classList.remove('show');
+            }
+        });
+    });
+
+    // Progress bar on scroll
+    window.addEventListener('scroll', () => {
+        const docHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+        const scrolled = (window.scrollY / docHeight) * 100;
+        document.getElementById('progress-bar').style.width = `${scrolled}%`;
+    });
+
+    // Animate sections on scroll
+    const sections = document.querySelectorAll('.section');
     const observer = new IntersectionObserver(entries => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
